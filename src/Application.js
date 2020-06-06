c7cf357f7643993fff9329e9ce29cd52fed47ee6
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-06-06 03:44:15 +0300

    Complete Container Pattern Solution

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Title from './Title';
 import Input from './Input';
@@ -7,56 +7,72 @@ import Result from './Result';
 import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
 
 const initialState = {
-  numberOfPeople: 10,
-  slicesPerPerson: 2,
+    numberOfPeople: 10,
+    slicesPerPerson: 2,
 };
 
-export default class Application extends Component {
-  state = { ...initialState };
-
-  updateNumberOfPeople = event => {
-    const numberOfPeople = parseInt(event.target.value, 10);
-    this.setState({ numberOfPeople });
-  };
-
-  updateSlicesPerPerson = event => {
-    const slicesPerPerson = parseInt(event.target.value, 10);
-    this.setState({ slicesPerPerson });
-  };
-
-  reset = event => {
-    this.setState({ ...initialState });
-  };
-
-  render() {
-    const { numberOfPeople, slicesPerPerson } = this.state;
-    const numberOfPizzas = calculatePizzasNeeded(
-      numberOfPeople,
-      slicesPerPerson,
-    );
-
-    return (
-      <div className="Application">
-        <Title />
-        <Input
-          label="Number of Guests"
-          type="number"
-          min={0}
-          value={numberOfPeople}
-          onChange={this.updateNumberOfPeople}
-        />
-        <Input
-          label="Slices Per Person"
-          type="number"
-          min={0}
-          value={slicesPerPerson}
-          onChange={this.updateSlicesPerPerson}
-        />
-        <Result amount={numberOfPizzas} />
-        <button className="full-width" onClick={this.reset}>
-          Reset
-        </button>
-      </div>
-    );
-  }
+class PizzaCalculator extends Component {
+    render() {
+        const {numberOfPeople, slicesPerPerson, numberOfPizzas, updateNumberOfPeople, updateSlicesPerPerson, reset} = this.props;
+        return (<div className="Application">
+            <Title/>
+            <Input
+                label="Number of Guests"
+                type="number"
+                min={0}
+                value={numberOfPeople}
+                onChange={updateNumberOfPeople}
+            />
+            <Input
+                label="Slices Per Person"
+                type="number"
+                min={0}
+                value={slicesPerPerson}
+                onChange={updateSlicesPerPerson}
+            />
+            <Result amount={numberOfPizzas}/>
+            <button className="full-width" onClick={reset}>
+                Reset
+            </button>
+        </div>);
+    }
 }
+
+export default class Application extends Component {
+    state = {...initialState};
+
+    updateNumberOfPeople = event => {
+        const numberOfPeople = parseInt(event.target.value, 10);
+        this.setState({numberOfPeople});
+    };
+
+    updateSlicesPerPerson = event => {
+        const slicesPerPerson = parseInt(event.target.value, 10);
+        this.setState({slicesPerPerson});
+    };
+
+    reset = event => {
+        this.setState({...initialState});
+    };
+
+    render() {
+        const {numberOfPeople, slicesPerPerson} = this.state;
+        const numberOfPizzas = calculatePizzasNeeded(
+            numberOfPeople,
+            slicesPerPerson,
+        );
+
+        return (
+            <PizzaCalculator
+                numberOfPeople={numberOfPeople}
+                slicesPerPerson={slicesPerPerson}
+                numberOfPizzas={numberOfPizzas}
+                updateNumberOfPeople={this.updateNumberOfPeople}
+                updateSlicesPerPerson={this.updateSlicesPerPerson}
+                reset={this.reset}
+            />
+        );
+    }
+}
+
+
